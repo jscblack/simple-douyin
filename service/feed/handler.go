f3406124c6f9d84021df9ef98131f2975b4688e9
Koschei <nitianzero@gmail.com>
2023-08-08 21:42:33 +0800

    refactor: improve Feed & Publish service logic

--- service/feed/handler.go
+++ service/feed/handler.go
@@ -5,7 +5,6 @@ import (
 	apiLog "github.com/prometheus/common/log"
 	feed "simple-douyin/kitex_gen/feed"
 	"simple-douyin/service/feed/service"
-	"time"
 )
 
 // FeedServiceImpl implements the last service interface defined in the IDL.
@@ -13,18 +12,13 @@ type FeedServiceImpl struct{}
 
 // Feed implements the FeedServiceImpl interface.
 func (s *FeedServiceImpl) Feed(ctx context.Context, req *feed.FeedRequest) (*feed.FeedResponse, error) {
-	// TODO: Your code here...
-
 	// 返回时间小于latestTime的最多30个视频
 	// 可以根据req.userId推荐个性化视频
-	if req.LatestTime == nil {
-		timeStamp := time.Now().Unix()
-		req.LatestTime = &timeStamp
-	}
 
 	resp, err := service.Feed(ctx, req)
 	if err != nil {
-		apiLog.Fatal(err)
+		apiLog.Error(err)
+		return nil, err
 	}
 
 	return resp, nil
--- service/publish/handler.go
+++ service/publish/handler.go
@@ -3,7 +3,7 @@ package main
 import (
 	"context"
 	servLog "github.com/prometheus/common/log"
-	publish "simple-douyin/kitex_gen/publish"
+	"simple-douyin/kitex_gen/publish"
 	"simple-douyin/service/publish/service"
 )
 
@@ -14,8 +14,21 @@ type PublishServiceImpl struct{}
 func (s *PublishServiceImpl) PublishAction(ctx context.Context, req *publish.PublishActionRequest) (resp *publish.PublishActionResponse, err error) {
 	resp, err = service.PublishAction(ctx, req)
 	if err != nil {
-		servLog.Fatal(err)
+		servLog.Info(err)
+		if resp == nil {
+			resp = publish.NewPublishActionResponse()
+		}
+		resp.StatusCode = 57003
+		if resp.StatusMsg == nil {
+			resp.StatusMsg = new(string)
+		}
+		*resp.StatusMsg = err.Error()
+		return
 	}
+	servLog.Info("Video Published Successfully.")
+	resp.StatusCode = 0
+	resp.StatusMsg = new(string)
+	*resp.StatusMsg = "Video Published Successfully."
 	return
 }
 
@@ -23,16 +36,63 @@ func (s *PublishServiceImpl) PublishAction(ctx context.Context, req *publish.Pub
 func (s *PublishServiceImpl) PublishList(ctx context.Context, req *publish.PublishListRequest) (resp *publish.PublishListResponse, err error) {
 	resp, err = service.PublishList(ctx, req)
 	if err != nil {
-		servLog.Fatal(err)
+		servLog.Info(err)
+		if resp == nil {
+			resp = publish.NewPublishListResponse()
+		}
+		resp.StatusCode = 57003
+		if resp.StatusMsg == nil {
+			resp.StatusMsg = new(string)
+		}
+		*resp.StatusMsg = err.Error()
+		return
 	}
-	return
+	resp.StatusCode = 0
+	resp.StatusMsg = new(string)
+	*resp.StatusMsg = "Get Video List Success."
+	return resp, nil
 }
 
 // PublishVideoInfo implements the PublishServiceImpl interface.
 func (s *PublishServiceImpl) PublishVideoInfo(ctx context.Context, req *publish.PublishVideoInfoRequest) (resp *publish.PublishVideoInfoResponse, err error) {
 	resp, err = service.PublishVideoInfo(ctx, req)
 	if err != nil {
-		servLog.Fatal(err)
+		servLog.Info(err)
+		if resp == nil {
+			resp = publish.NewPublishVideoInfoResponse()
+		}
+		resp.StatusCode = 57003
+		if resp.StatusMsg == nil {
+			resp.StatusMsg = new(string)
+		}
+		*resp.StatusMsg = err.Error()
+		return
 	}
+	servLog.Info("Get Video Info Success.")
+	resp.StatusCode = 0
+	resp.StatusMsg = new(string)
+	*resp.StatusMsg = "Get Video Info Success."
+	return
+}
+
+// PublishWorkCount implements the PublishServiceImpl interface.
+func (s *PublishServiceImpl) PublishWorkCount(ctx context.Context, req *publish.PublishWorkCountRequest) (resp *publish.PublishWorkCountResponse, err error) {
+	resp, err = service.PublishWorkCount(ctx, req)
+	if err != nil {
+		servLog.Info(err)
+		if resp == nil {
+			resp = publish.NewPublishWorkCountResponse()
+		}
+		resp.StatusCode = 57003
+		if resp.StatusMsg == nil {
+			resp.StatusMsg = new(string)
+		}
+		*resp.StatusMsg = err.Error()
+		return
+	}
+	servLog.Info("Get Work Count Success.")
+	resp.StatusCode = 0
+	resp.StatusMsg = new(string)
+	*resp.StatusMsg = "Get Work Count Success."
 	return
 }
