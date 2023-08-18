99c1505ff26d9de822c625bc945e26533ffaa7fb
jscblack <jscblack@163.com>
2023-08-19 00:16:33 +0800

    feat: add start.sh

---
+++ service/start.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Start the user service
+go run ./user/. &
+
+# Start the feed service
+go run ./feed/. &
+
+# Start the publish service
+go run ./publish/. &
+
+# Start the favorite service
+go run ./favorite/. &
+
+# Start the relation service
+go run ./relation/. &
+
+# Start the comment service
+go run ./comment/. &
+
+# Wait for all background processes to finish
+wait
